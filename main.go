964045359e6c15639a8728df271d3bf070e1dd81
Logan <lgntravis@gmail.com>
2025-01-20 09:35:51 -0700

    Removing comments

--- main.go
+++ main.go
@@ -9,14 +9,12 @@ import (
 func main() {
 	app := fiber.New()
 
-	// Serve a simple "Hello, World!" message at the root route
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello, World!")
 	})
 
-	// Serve the resume.pdf file at the /resume route
 	app.Get("/resume", func(c *fiber.Ctx) error {
-		return c.SendFile("./resume.pdf") // Path to your resume.pdf
+		return c.SendFile("./resume.pdf")
 	})
 
 	log.Fatal(app.Listen(":3000"))
