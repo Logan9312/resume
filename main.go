f4ed0fa8a75c18937ad3b3cdf8a2e2efddbdc440
Logan <lgntravis@gmail.com>
2025-01-20 09:47:55 -0700

    Serving PDF

--- main.go
+++ main.go
@@ -10,10 +10,6 @@ func main() {
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World!")
-	})
-
-	app.Get("/resume", func(c *fiber.Ctx) error {
 		return c.SendFile("./resume.pdf")
 	})
 
